41e0b8fc42cbae986a893246aa3be82f8d85f4a3
millerarocha <millerarocha@gmail.com>
2024-04-30 12:07:11 -0500

    fix[cat-app]: fix apos on description text

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,9 @@ function App() {
       <h1 className={styles.mainTitle}>Cat App </h1>
       <p className={styles.mainDescription}>
         Cat App is a delightful platform that showcases a curated collection of 20 adorable cats from around the world.
-        Each cat is beautifully displayed on a card, featuring captivating images and brief descriptions. Whether you're
-        a cat lover or simply appreciate their charm, Cat App offers a delightful experience to explore and enjoy these
-        furry companions.
+        Each cat is beautifully displayed on a card, featuring captivating images and brief descriptions. Whether
+        you&apos;re a cat lover or simply appreciate their charm, Cat App offers a delightful experience to explore and
+        enjoy these furry companions.
       </p>
       {loading && <h1>Loading data...</h1>}
       <div className={styles.cardContainer}>
